410cb7bca8279d94449b22597d834e7ef5fc72b7
Soumya Nadegouni <nsoumya.developer@gmail.com>
2017-09-27 19:14:24 -0700

    Updated index view to use partial in order to display list of posts
    Updated index view to use partial in order to display list of posts

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,9 @@
 require_relative '../helpers/search_helper'
 class SearchController < ApplicationController
   include SearchHelper
+  # def initialize
+  #   @posts = nil
+  # end
   def index
     elasticsearch = ElasticSearchRestApi.new
     results = elasticsearch.posts('How to')#filters
