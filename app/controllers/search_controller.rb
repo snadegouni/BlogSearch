7858a78d7e33bd1948ab07fa3e054ed68ee42a93
Soumya Nadegouni <nsoumya.developer@gmail.com>
2017-09-28 20:15:37 -0700

    Added code to use input search critieria.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,12 +1,15 @@
 require_relative '../helpers/search_helper'
 class SearchController < ApplicationController
   include SearchHelper
-  # def initialize
-  #   @posts = nil
-  # end
+ 
   def index
+   
+  end
+  def show
     elasticsearch = ElasticSearchRestApi.new
     results = elasticsearch.posts('How to')#filters
     @posts = results
+    puts 'I am in show'
+    render 'index'
   end
 end
